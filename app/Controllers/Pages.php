2d1145f26b282d8634b5506a1741b98bd1a41dc9
nd1rh <ainanadirah613@gmail.com>
2026-01-10 18:15:54 +0800

    update

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -10,10 +10,18 @@ class Pages extends BaseController
         echo view('home_view'); 
         echo view('templates/footer'); 
     }
+
     public function webPolicy()
     {
         echo view('templates/header'); 
         echo view('web_policy'); 
+        echo view('templates/footer'); 
+    }
+
+    public function about()
+    {
+        echo view('templates/header'); 
+        echo view('about_view'); 
         echo view('templates/footer'); 
     }
 
